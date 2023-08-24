512d00b16a8ee491b62a1468c0481a7396f96f28
kumudiaorong <kmdr.error@gmail.com>
2023-08-24 19:17:33 +0800

    v0.0.4 log

--- src/core.cpp
+++ src/core.cpp
@@ -3,9 +3,14 @@
 #include "comm.h"
 #include "core.h"
 #include "dialog.h"
+#include "spdlog/async.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/spdlog.h"
 namespace qst {
   QstFrontendCore::QstFrontendCore(int& argc, char *argv[])
-    : QApplication(argc, argv) {
+    : QApplication(argc, argv)
+    , logger(spdlog::create_async<spdlog::sinks::stdout_color_sink_mt>("backend")) {
+    spdlog::set_default_logger(logger);
     this->setApplicationDisplayName("Qst");
     for(int i = 1; i < argc; i++) {
       if(strcmp(argv[i], "--addr") == 0) {
@@ -17,5 +22,6 @@ namespace qst {
     QObject::connect(&dialog, &Dialog::runApp, &comm, &Comm::runApp);
     dialog.show();
   }
-  QstFrontendCore::~QstFrontendCore(){}
+  QstFrontendCore::~QstFrontendCore() {
+  }
 }  // namespace qst
